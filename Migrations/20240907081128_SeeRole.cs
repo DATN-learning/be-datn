552b0b388bb7768a724ee2af8dced4656e77b3ba
vietthoitsme <nvtho.20it1@gmail.com>
2024-09-07 22:18:18 +0700

    first commit

---
+++ Migrations/20240907081128_SeeRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeeRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1aecdb3c-5ccc-4e1b-b986-3d6a25e648c0", null, "User", "USER" },
+                    { "c50b28d1-d91a-4d6f-b213-475e988acb8e", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1aecdb3c-5ccc-4e1b-b986-3d6a25e648c0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c50b28d1-d91a-4d6f-b213-475e988acb8e");
+        }
+    }
+}
